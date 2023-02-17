0fade41717cd0b82e78a3afa5d699e62a5780531
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 22:20:06 +0530

    Added payment entity

--- back_end/src/main/java/lk/ijse/spring/entity/Payment.java
+++ back_end/src/main/java/lk/ijse/spring/entity/Payment.java
@@ -1,4 +1,29 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
 public class Payment {
+    @Id
+    private String paymentId;
+    private LocalDate date;
+    private double amount;
+    private double damaged_cost;
+    private String damagedDescription;
+    private long extraMileage;
+    private long costPerExtraKm;
+    private String paymentStatus;
+
+    @OneToOne
+    private Rental rentalId;
 }
