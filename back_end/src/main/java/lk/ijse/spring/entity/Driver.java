abb7b2234cc89e16e61912da126fe2ab9e713786
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 22:18:15 +0530

    Added Driver Entity

--- back_end/src/main/java/lk/ijse/spring/entity/Driver.java
+++ back_end/src/main/java/lk/ijse/spring/entity/Driver.java
@@ -1,4 +1,29 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
 public class Driver {
+    @Id
+    private String driverId;
+    private String name;
+    private String nic;
+    private String drivingLicenseNumber;
+    private LocalDate dob;
+    private String status;
+
+    @OneToMany(mappedBy = "driverId")
+    private List<RentalDetail> rentalDetailsList;
 }
+
