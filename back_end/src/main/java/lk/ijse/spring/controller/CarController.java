10a23bbaa9770281ba6f4c321eabee95ae8fdf7f
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 23:54:08 +0530

    added Controller

--- back_end/src/main/java/lk/ijse/spring/controller/CarController.java
+++ back_end/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -1,4 +1,11 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/car")
 public class CarController {
 }
