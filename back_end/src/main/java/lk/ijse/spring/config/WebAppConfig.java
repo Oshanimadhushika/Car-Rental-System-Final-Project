ccba1b3371e7f230da346a25c22ffcf4892fd8f9
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 19:44:57 +0530

    Added backend

---
+++ back_end/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring")
+@EnableWebMvc
+public class WebAppConfig {
+}
