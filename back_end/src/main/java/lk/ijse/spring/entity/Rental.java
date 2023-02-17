949d0c8e253931b7dea584e711ee5009948676fa
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 22:22:32 +0530

    added rental entity

--- back_end/src/main/java/lk/ijse/spring/entity/Rental.java
+++ back_end/src/main/java/lk/ijse/spring/entity/Rental.java
@@ -1,4 +1,28 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
 public class Rental {
+    @Id
+    private String rentalId;
+    @ManyToOne
+    private Customer customerId;
+    private LocalDate date;
+    private LocalDate pickupDate;
+    private double amount;
+    private LocalDate returnDate;
+    private double total_damage_viewer_payment;
+    private String pickupLocation;
+    private String returnLocation;
 }
