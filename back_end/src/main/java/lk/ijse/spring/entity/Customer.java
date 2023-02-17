d06bbb3751e4b4eba97eaf56a8cba4857e0e6c43
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 22:15:17 +0530

    Added customer entity

--- back_end/src/main/java/lk/ijse/spring/entity/Customer.java
+++ back_end/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,4 +1,30 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
 public class Customer {
+    @Id
+    private String customerId;
+    private String name;
+    private String address;
+    private String nic;
+    private String drivingLicenseNumber;
+    private String contactNumber;
+    private String email;
+    private String password;
+    private String imageLocation;
+
+    @OneToMany(mappedBy = "customerId")
+    private List<Rental> rentalDetails;
 }
