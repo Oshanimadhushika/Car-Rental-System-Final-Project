68015559a6957a58c773cd5e5cfa29334f02ed32
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 21:36:08 +0530

    Added backend

--- back_end/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ back_end/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -7,11 +7,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
