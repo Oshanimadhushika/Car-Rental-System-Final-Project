adb49a5813a630dc55be5265c073ddf58971c85c
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-17 22:07:10 +0530

    Added admin entity

--- back_end/src/main/java/lk/ijse/spring/entity/Admin.java
+++ back_end/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
 public class Admin {
+    @Id
+    private String adminId;
+    private String email;
+    private String userName;
+    private String password;
 }
