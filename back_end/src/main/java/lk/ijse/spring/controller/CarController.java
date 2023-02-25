3db3c8f00ce00a6f7b887465c16fed0413322164
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-25 12:22:40 +0530

    Added car controller post mapping

--- back_end/src/main/java/lk/ijse/spring/controller/CarController.java
+++ back_end/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -1,11 +1,23 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/car")
 public class CarController {
+
+    @Autowired
+    CarService carService;
+
+    @PostMapping
+    public ResponseUtil saveCar(@RequestBody CarDTO dto){
+        carService.saveCar(dto);
+        return new ResponseUtil("200", "Registration Successfully....", dto);
+    }
+
 }
