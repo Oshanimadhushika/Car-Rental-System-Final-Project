118e56384e3c2610339fb4d3f8d7cfd10ec00948
oshani madhushika <oshanimadhushika12@gmail.com>
2023-02-28 11:24:13 +0530

    Added driver login entity

--- back_end/src/main/java/lk/ijse/spring/entity/DriverLogin.java
+++ back_end/src/main/java/lk/ijse/spring/entity/DriverLogin.java
@@ -1,4 +1,19 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
 public class DriverLogin {
+    @Id
+    private String driverId;
+    private String userName;
+    private String password;
 }
