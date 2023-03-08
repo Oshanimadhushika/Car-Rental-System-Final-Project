2de18708bb1ad2d13082f97f34fff744085d1304
oshani madhushika <oshanimadhushika12@gmail.com>
2023-03-08 20:19:07 +0530

    added query

--- back_end/src/main/java/lk/ijse/spring/repo/RentalRepo.java
+++ back_end/src/main/java/lk/ijse/spring/repo/RentalRepo.java
@@ -4,6 +4,7 @@ import lk.ijse.spring.entity.Driver;
 import lk.ijse.spring.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +15,7 @@ public interface RentalRepo extends JpaRepository<Rental,String> {
 
     @Query(value = "SELECT rentalId FROM rental ORDER BY rentalId DESC limit 1", nativeQuery = true)
     String generateReservationId();
+
+    @Query(value = "SELECT * FROM rental WHERE customerId=:customerId", nativeQuery = true)
+    List<Rental> getRentalByReservationStatus(@Param("customerId") String customerId);
 }
